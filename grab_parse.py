a05bd4d659b236b52d8b416957e36f74b5d2de58
EricSchles <Ericschles@gmail.com>
2015-07-16 16:23:36 -0400

    Update grab_parse.py

--- grab_parse.py
+++ grab_parse.py
@@ -1,6 +1,16 @@
 import requests
 import lxml.html
-
+import gzip
+#method for reading straight xml
 r= requests.get("http://smarkets.s3.amazonaws.com/oddsfeed.xml")
 e = lxml.html.etree.parse(r.text.encode("ascii","ignore"))
 print dir(e)
+
+r = requests.get("http://thedataincubator-challenge.s3.amazonaws.com/HCHACdrgRBoVokdgdJNF/Badges.xml.gz")
+with open("badges.xml.gz", "wb") as f:
+  f.write(r.content())
+g = gzip.open("badges.xml.gz","rb") 
+
+with open("badges.xml","w") as f:
+  f.write(g.read())
+
